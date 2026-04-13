95ecc67ad1a4e137dd1b70617322b533f6f6259e
sinfrataxigo <sinfrataxigo@gmail.com>
2026-04-13 13:19:21 +0000

    Create sw.js file for Service Worker functionality

---
+++ sw.js
@@ -0,0 +1,45 @@
+// sw.js - Service Worker for offline functionality and caching
+
+// Cache name and version
+const CACHE_NAME = 'v1';
+
+// Files to cache
+const FILES_TO_CACHE = [
+    '/index.html',
+    '/styles.css',
+    '/script.js'
+];
+
+// Install event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
